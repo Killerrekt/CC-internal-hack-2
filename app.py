c9656a886d07ecb37268e52e968e8472ae2f2e49
Killerrekt <86644389+Killerrekt@users.noreply.github.com>
2023-05-21 17:12:00 +0530

    final version hopefully

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, url_for
 from markupsafe import escape
 import mysql.connector
 
@@ -11,7 +11,11 @@ cur.execute('use clubs')
 if __name__=="__main__":
     app.run(debug=True)
 
-@app.route("/", methods = ["POST","GET"])
+@app.route("/")
+def rend():
+    return render_template("index.html")
+
+@app.route("/events", methods = ["POST","GET"])
 def events():
     cur.execute("select distinct club from event")
     club = cur.fetchall()
@@ -28,12 +32,16 @@ def events():
         clb = clb.strip()
         cur.execute("select * from event where club = '"+clb+"'")
         details = cur.fetchall()
-        return render_template('login.html',club = club, check = details)
+        return render_template('events.html',club = club, check = details)
     else:
-        return render_template('login.html',club = club)
+        return render_template('events.html',club = club)
 
 
-@app.route('/transfer')
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/mail')
 def transfer():
     cur.execute("select * from transfer")
     data = cur.fetchall()
@@ -45,12 +53,8 @@ def transfer():
     a = a.replace("\\r\\n"," ")
     data = a.split('!')
     data = data[:-1]
-    return render_template('index.html',transfer = data)
-
-@app.route('/internship')
-def intern():
     cur.execute("select * from internship")
-    data = cur.fetchall()
+    dat = cur.fetchall()
     a = str(data)
     print(a)
     a = a.replace("('","")
@@ -59,6 +63,6 @@ def intern():
     a = a.replace("[","")
     a = a.replace("]","")
     a = a.replace("\\r\\n"," ")
-    data = a.split('?')
-    data = data[:-1]
-    return render_template('intern.html',intern = data)
+    dat = a.split('?')
+    dat = data[:-1]
+    return render_template('mail.html',intern = dat, transfer = data)
